c92281b4f95794609ddf9b2a219f9e71081454e7
ouattarahodou <83280873+ouattarahodou@users.noreply.github.com>
2022-06-03 20:11:35 +0200

    ajout des fichiers js

--- audio/script.js
+++ audio/script.js
@@ -1,2 +1,39 @@
 
+// déclaration de variable de stockage
 console.log("clique 1 fois");
+console.log("je t'aime mon amour à moi seul");
+
+var age;
+age=25
+
+var name= "chérie"
+console.log(name);
+
+var firstname= "boumouboum"
+console.log(firstname);
+
+// LES STRUCTURES CONDITIONNELLES
+var age= 50; // prompt() a la place de chiffre 
+if (age> 18) {
+    console.log("vous etes majeur");
+    
+}else{
+    console.log("vous etes mineur");
+}
+
+// LES BOULES FOR 
+var index =0;
+while (index < 5){
+    console.log("Tour de boucle while n° "+index);
+    index++;
+}
+
+// do while
+var index =0;
+do{
+    console.log("Tour de boucle dowhile n° "+index);
+    index++;
+} while (index <4);
+
+
+
